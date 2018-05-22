6bb852c5d5d816cae6992b3181cd8550f99ebc56
Kadin-Backlund <39378257+Kadin-Backlund@users.noreply.github.com>
2018-05-22 12:59:43 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+function Alert() {
+alert("Welcome to my website about my favorite singer of all time... Billy Joel !!! This website will give you a brief overview of Billy Joel and his career! I hope you enjoy it!");
+}
+
+alert("Welcome to Kadin Backlund's webpage on Billy Joel!");
+
+console.log("Hello Wrold from Javascript");
+
